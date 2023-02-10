e032bcb5afe977709a641eccf38f65bc67baa746
ardririy <hynnkgw27@gmail.com>
2023-02-10 16:46:06 +0900

    動かしてみた

--- deno_get_started/first_steps.ts
+++ deno_get_started/first_steps.ts
@@ -1,1 +1,5 @@
-console.log("welcome to Deno!");
+const filenames = Deno.args;
+for (const filename of filenames) {
+  const file = await Deno.open(filename);
+  await file.readable.pipeTo(Deno.stdout.writable);
+}
